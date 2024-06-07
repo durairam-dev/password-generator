846107fcd2610406bbe3c1baa383ef7d8738f570
durairam-dev <durairam1221@gmail.com>
2024-06-08 01:47:36 +0530

    Custom password generator

---
+++ src/utils/script.js
@@ -0,0 +1,38 @@
+function clickGenerateButton() {
+  const length = document.getElementById("passwordLength").value;
+  const includeUppercase = document.getElementById("includeUppercase").checked;
+  const includeNumbers = document.getElementById("includeNumbers").checked;
+  const includeSymbols = document.getElementById("includeSymbols").checked;
+  document.getElementById("generatedPassword").value = generatePassword(
+    length,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+  );
+}
+
+function generatePassword(
+  length,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
+) {
+  const lowercase = "abcdefghijklmnopqrstuvwxyz";
+  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbers = "0123456789";
+  const symbols = "!@#$%^&*()_+[]{}|;:,.<>?";
+
+  let characters = lowercase;
+  if (includeUppercase) characters += uppercase;
+  if (includeNumbers) characters += numbers;
+  if (includeSymbols) characters += symbols;
+
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+
+  return password;
+}
